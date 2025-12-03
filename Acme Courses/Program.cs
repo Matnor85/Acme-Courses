5f309d5274e50d9dab2f3bb2f11c0d15f6455419
Mathias Norrback <mathias.norrbacken@gmail.com>
2025-12-03 13:17:14 +0100

    Add project files.

---
+++ Acme Courses/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Acme_Courses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
