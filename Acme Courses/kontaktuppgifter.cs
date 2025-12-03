1b1ad22a302be3b26d3cde468bffd1e483736f6c
Oscar Svahn <oscar@svahndata.se>
2025-12-03 15:00:11 +0100

    Added Kontaktuppgifter.

---
+++ Acme Courses/kontaktuppgifter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Acme_Courses;
+
+public class kontaktuppgift
+{
+    public int ID { get; set; }
+    public string KontaktTyp { get; set; } = null!;
+    public string KontaktUppgift { get; set; } = null!;
+    public int ElevID{ get; set; }
+}
