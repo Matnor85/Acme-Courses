192a774604df953b3678355628972f0aed910256
Mathias Norrback <mathias.norrbacken@gmail.com>
2025-12-06 00:16:47 +0100

    Ny databas på G

--- Acme Courses/Migrations/20251205231609_NewTables.Designer.cs
+++ Acme Courses/Migrations/20251205231609_NewTables.Designer.cs
@@ -4,6 +4,7 @@ using Acme_Courses;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Acme_Courses.Migrations
 {
     [DbContext(typeof(ApplicationContext))]
-    partial class ApplicationContextModelSnapshot : ModelSnapshot
+    [Migration("20251205231609_NewTables")]
+    partial class NewTables
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Acme Courses/Migrations/20251205231609_NewTables.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Acme_Courses.Migrations
+{
+    /// <inheritdoc />
+    public partial class NewTables : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateOnly>(
+                name: "StartDatum",
+                table: "Kurser",
+                type: "date",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.AlterColumn<DateOnly>(
+                name: "SlutDatum",
+                table: "Kurser",
+                type: "date",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.UpdateData(
+                table: "Kurser",
+                keyColumn: "ID",
+                keyValue: 1,
+                columns: new[] { "SlutDatum", "StartDatum" },
+                values: new object[] { new DateOnly(2025, 12, 15), new DateOnly(2025, 9, 1) });
+
+            migrationBuilder.UpdateData(
+                table: "Kurser",
+                keyColumn: "ID",
+                keyValue: 2,
+                columns: new[] { "SlutDatum", "StartDatum" },
+                values: new object[] { new DateOnly(2025, 11, 30), new DateOnly(2025, 9, 1) });
+
+            migrationBuilder.UpdateData(
+                table: "Kurser",
+                keyColumn: "ID",
+                keyValue: 3,
+                columns: new[] { "SlutDatum", "StartDatum" },
+                values: new object[] { new DateOnly(2025, 12, 1), new DateOnly(2025, 9, 1) });
+
+            migrationBuilder.UpdateData(
+                table: "Kurser",
+                keyColumn: "ID",
+                keyValue: 4,
+                columns: new[] { "SlutDatum", "StartDatum" },
+                values: new object[] { new DateOnly(2025, 12, 20), new DateOnly(2025, 10, 1) });
+
+            migrationBuilder.UpdateData(
+                table: "Kurser",
+                keyColumn: "ID",
+                keyValue: 5,
+                columns: new[] { "SlutDatum", "StartDatum" },
+                values: new object[] { new DateOnly(2026, 1, 15), new DateOnly(2025, 9, 15) });
+
+            migrationBuilder.UpdateData(
+                table: "Kurser",
+                keyColumn: "ID",
+                keyValue: 6,
+                columns: new[] { "SlutDatum", "StartDatum" },
+                values: new object[] { new DateOnly(2026, 2, 1), new DateOnly(2025, 11, 1) });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "StartDatum",
+                table: "Kurser",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(DateOnly),
+                oldType: "date");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "SlutDatum",
+                table: "Kurser",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(DateOnly),
+                oldType: "date");
+
+            migrationBuilder.UpdateData(
+                table: "Kurser",
+                keyColumn: "ID",
+                keyValue: 1,
+                columns: new[] { "SlutDatum", "StartDatum" },
+                values: new object[] { new DateTime(2025, 12, 15, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2025, 9, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) });
+
+            migrationBuilder.UpdateData(
+                table: "Kurser",
+                keyColumn: "ID",
+                keyValue: 2,
+                columns: new[] { "SlutDatum", "StartDatum" },
+                values: new object[] { new DateTime(2025, 11, 30, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2025, 9, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) });
+
+            migrationBuilder.UpdateData(
+                table: "Kurser",
+                keyColumn: "ID",
+                keyValue: 3,
+                columns: new[] { "SlutDatum", "StartDatum" },
+                values: new object[] { new DateTime(2025, 12, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2025, 9, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) });
+
+            migrationBuilder.UpdateData(
+                table: "Kurser",
+                keyColumn: "ID",
+                keyValue: 4,
+                columns: new[] { "SlutDatum", "StartDatum" },
+                values: new object[] { new DateTime(2025, 12, 20, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2025, 10, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) });
+
+            migrationBuilder.UpdateData(
+                table: "Kurser",
+                keyColumn: "ID",
+                keyValue: 5,
+                columns: new[] { "SlutDatum", "StartDatum" },
+                values: new object[] { new DateTime(2026, 1, 15, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2025, 9, 15, 0, 0, 0, 0, DateTimeKind.Unspecified) });
+
+            migrationBuilder.UpdateData(
+                table: "Kurser",
+                keyColumn: "ID",
+                keyValue: 6,
+                columns: new[] { "SlutDatum", "StartDatum" },
+                values: new object[] { new DateTime(2026, 2, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2025, 11, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) });
+        }
+    }
+}
