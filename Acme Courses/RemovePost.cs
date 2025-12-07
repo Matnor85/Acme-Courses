0b2acc6b65ff86adc7279a39208cdb41c95f4adb
Oscar Svahn <oscar@svahndata.se>
2025-12-07 03:17:54 +0100

    Jag har fixat några problem med den tidigare koden i RemovePost();, men det behövs ta en titt på hur vi sedan sparar ändringen.

--- Acme Courses/RemovePost.cs
+++ Acme Courses/RemovePost.cs
@@ -1,34 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
 using System.Text;
 
 namespace Acme_Courses;
 
 public class RemovePost()
 {
-    //string tabell = tabellNamn;
-
     static ApplicationContext context = new ApplicationContext();
-
 
     public static void RemoveFrom()
     {
-        UserChoice();
-        var förnamn = UserChoice().förnamn;
-        var efternamn = UserChoice().efternamn;
+        var namn = UserChoice();
+        Console.WriteLine(namn.förnamn);
         var std = context.Elever
             .Select(e => new { e.Förnamn, e.Efternamn })
-            .Where(e => e.Förnamn == förnamn && e.Efternamn == efternamn);
-
+            .Where(e => e.Förnamn == namn.förnamn && e.Efternamn == namn.efternamn);
 
         if (std == null)
         {
             ConsoleHelper.CenterAll("Student not found!");
             return;
         }
-
-        //context.Elever.Remove(std);
 
         AreYouSure();
     }
@@ -44,11 +38,10 @@ public class RemovePost()
         List<int> ID = new List<int>();
         foreach (var item in q)
         {
-            elevList.Add(string.Join($" {item.Förnamn}", $"{item.ID}", $"{item.Efternamn}"));
+            elevList.Add(string.Join($" {item.Förnamn} ", $"{item.ID}", $"{item.Efternamn}"));
             ID.Add(item.ID);
         }
         ConsoleHelper.CenterBlock(elevList);
-        //ConsoleHelper.CenterMenu();
 
         ConsoleHelper.CenterAll("Enter the number of the row you wish to edit.");
         ConsoleKeyInfo key = Console.ReadKey(true);
@@ -57,10 +50,20 @@ public class RemovePost()
         {
             if (yada <= ID.Count && yada >= 0)
             {
-                var que = context.Elever
-                    .Select(x => new { x.ID, x.Förnamn, x.Efternamn })
-                    .Where(x => x.ID == int.Parse(key.ToString()!));
-                return (q.First().Förnamn, q.First().Efternamn);
+                //Hämtar listan ifrån den övre queryn och sedan specifierar vilket namn vi vill ha med yada ifrån TryParsen åvan.
+                var namn = elevList[yada];   
+                var namnParts = namn.Split(' ');
+                //delar upp stringen så vi kan använda den till queryn i RemoveFrom();
+                if (namnParts.Length >= 3)
+                {
+                    string förnamn = namnParts[1];
+                    string efternamn = namnParts[2];
+                    return (förnamn, efternamn);
+                }
+                else
+                {
+                    throw new Exception("Invalid name format");
+                }
             }
             else
             {
@@ -70,22 +73,21 @@ public class RemovePost()
             }
             throw new Exception("Sitt på en kaktus");
         }
-
         throw new Exception("Sitt på en kaktus");
     }
 
     private static void AreYouSure()
     {
         Console.Clear();
-        ConsoleHelper.CenterAll("Are you sure?: [Y] = YES or [N] for no");
+        ConsoleHelper.CenterAll("Are you sure?: [Y] for YES or [N] for no");
         Console.WriteLine();
         Console.WriteLine();
 
         ConsoleKeyInfo key = Console.ReadKey(true);
-
-        switch (char.ToLower(key.KeyChar))
-        {
+            switch (char.ToLower(key.KeyChar))
+            {
             case 'y':
+                //this save not be saving. . .
                 context.SaveChanges();
                 break;
             case 'n':
